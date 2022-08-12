5673cc3fe6129b3bfc295d3a0d8a274bc667f660
javekk <periniraffaele@gmail.com>
2022-08-12 15:32:16 +0200

    game of lights

--- stpong/shader/simplestvert.glsl
+++ stpong/shader/simplestvert.glsl
@@ -3,11 +3,13 @@
 layout (location = 0) in vec3 aPos;   // the position variable has attribute position 0
 layout (location = 1) in vec3 aColor; // the color variable has attribute position 1
 
-out vec3 ourColor; // output a color to the fragment shader
+out vec3 ourColor; 
+out vec3 ourPosition; 
 
 uniform float xOffset;
 
 void main(){
     gl_Position = vec4(aPos.x + xOffset, aPos.y, aPos.z, 1.0);
-    ourColor = aColor; // set ourColor to the input color we got from the vertex data
+    ourColor = aColor; 
+    ourPosition = aPos + xOffset;
 }
