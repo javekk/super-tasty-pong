b1ba0913c64b43220402fdfca15cd0c8b5761fdb
javekk <periniraffaele@gmail.com>
2022-08-31 21:33:25 +0200

    Make the texture funkier

--- stpong/shader/simplestfrag.glsl
+++ stpong/shader/simplestfrag.glsl
@@ -5,7 +5,8 @@ in vec3 ourColor;
 in vec2 TexCoord;
 
 uniform sampler2D ourTexture;
+uniform float xOffset;
 
 void main(){
-    FragColor = texture(ourTexture, TexCoord);
+    FragColor = texture(ourTexture, TexCoord) * vec4(ourColor + xOffset, 1.0); 
 }
