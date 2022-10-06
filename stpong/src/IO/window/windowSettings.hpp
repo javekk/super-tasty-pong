4776b6aab86b0c45c3d7d01199965534d85cb180
javekk <periniraffaele@gmail.com>
2022-10-06 22:00:27 +0200

    Minor changes

--- stpong/src/IO/window/windowSettings.hpp
+++ stpong/src/IO/window/windowSettings.hpp
@@ -1,15 +1,11 @@
-// Preprocessor directives
 #ifndef WINDOW_SETTINGS_HPP
 #define WINDOW_SETTINGS_HPP
 #pragma once
 
-// System Headers
 #include <glad/glad.h>
 
-// Standard headers
 #include <string>
 
-// Constants
 const int         windowWidth     = 1024;
 const int         windowHeight    = 768;
 const std::string windowTitle     = "Super Tasty Pong";
